447401a19d3a352da8d361aa8d0c1d7d50022b88
Caprena1 <robcaprena@gmail.com>
2024-10-10 14:19:32 -0400

    Adding joins and filtering data_preliminary

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,18 @@
--- Add your sql here
+SELECT 
+    customers.customer_name AS "Customer", '$' || PRINTF('%.2f', SUM(subscriptions.price_per_month * subscriptions.subscription_length)) AS 'Total Amount Due'
+FROM 
+    customers
+JOIN 
+    subscriptions 
+ON 
+    customers.customer_id = subscriptions.subscription_id
+LEFT JOIN 
+    orders
+ON 
+    subscriptions.subscription_id = orders.subscription_id
+WHERE 
+    subscriptions.description = 'Fashion Magazine'
+    AND orders.order_status = 'unpaid'
+GROUP BY 
+    customers.customer_name
+	
